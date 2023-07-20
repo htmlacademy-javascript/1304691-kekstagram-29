96c3aeb1f937596cc3649bd2873b675702755961
Divashka <dianakuzmina325@gmail.com>
2023-07-20 13:45:33 +0300

    9.15. Помощь друга

---
+++ js/effects.js
@@ -0,0 +1,145 @@
+const effectLevelValueNode = document.querySelector('.effect-level__value');
+const imgUploadPreviewNode = document.querySelector('.img-upload__preview');
+const effectsList = document.querySelector('.effects__list');
+
+const configEffects = {
+  default: {
+    range: {
+      'min': 0,
+      'max': 100
+    },
+    start: 0,
+    step: 1,
+    connect: 'lower',
+  },
+  chrome: {
+    range: {
+      'min': 0,
+      'max': 1
+    },
+    start: 0,
+    step: 0.1,
+    connect: 'lower',
+  },
+  sepia: {
+    range: {
+      'min': 0,
+      'max': 1
+    },
+    start: 0,
+    step: 0.1,
+    connect: 'lower',
+  },
+  marvin: {
+    range: {
+      'min': 0,
+      'max': 100
+    },
+    start: 0,
+    step: 1,
+    connect: 'lower',
+  },
+  phobos: {
+    range: {
+      'min': 0,
+      'max': 3
+    },
+    start: 0,
+    step: 0.1,
+    connect: 'lower',
+  },
+  heat: {
+    range: {
+      'min': 1,
+      'max': 3
+    },
+    start: 0,
+    step: 0.1,
+    connect: 'lower',
+  }
+};
+
+const effects = {
+  chrome: 'grayscale',
+  sepia: 'sepia',
+  marvin: 'invert',
+  phobos: 'blur',
+  heat: 'brightness',
+};
+
+const effectsUnits = {
+  chrome: '',
+  sepia: '',
+  marvin: '%',
+  phobos: 'px',
+  heat: '',
+};
+
+let chosenEffect = 'none';
+
+const sliderNode = document.querySelector('.img-upload__effect-level');
+
+const createImageStyle = () => {
+  if (chosenEffect === 'none') {
+    imgUploadPreviewNode.style.filter = null;
+    return;
+  }
+
+  const { value } = effectLevelValueNode;
+  const currentEffect = effects[chosenEffect];
+  const currentEffectUnit = effectsUnits[chosenEffect];
+  imgUploadPreviewNode.style.filter = `${currentEffect}(${value}${currentEffectUnit})`;
+};
+
+const destroySlider = () => {
+  if (sliderNode.noUiSlider) {
+    sliderNode.noUiSlider.destroy();
+  }
+  createImageStyle();
+};
+
+const onSliderUpdate = () => {
+  effectLevelValueNode.value = sliderNode.noUiSlider.get();
+  createImageStyle();
+};
+
+const createSlider = ({ range, step }) => {
+  noUiSlider.create(sliderNode, {
+    range: {
+      'min': range.min,
+      'max': range.max
+    },
+    start: 0,
+    step,
+    connect: 'lower',
+  });
+  sliderNode.noUiSlider.on('update', onSliderUpdate);
+};
+
+const setSlider = () => {
+  destroySlider();
+  sliderNode.classList.add('hidden');
+
+  if (chosenEffect !== 'none') {
+    createSlider(configEffects[chosenEffect]);
+    sliderNode.classList.remove('hidden');
+  }
+};
+
+const createEffect = (effect) => {
+  chosenEffect = effect;
+  setSlider();
+};
+
+const reset = () => {
+  createEffect('none');
+};
+
+const init = () => {
+  setSlider();
+  effectsList.addEventListener('change', (evt) => {
+    createEffect(evt.target.value);
+  });
+};
+
+export { init, reset };
--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 import { getPictures } from './data.js';
 import { renderPicturesGallery } from './gallery.js';
 import { openUploadForm } from './form.js';
+import { init as initSlider } from './effects.js';
+import { init as initScaleControl } from './scaler.js';
 
 renderPicturesGallery(getPictures());
 
 openUploadForm();
-
+initScaleControl();
+initSlider();
