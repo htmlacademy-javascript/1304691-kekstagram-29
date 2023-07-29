025205e6ac6bddde893521bf45c28296593c4197
Divashka <dianakuzmina325@gmail.com>
2023-07-29 14:01:54 +0300

    12.13. Фото на память

--- js/form.js
+++ js/form.js
@@ -3,9 +3,12 @@ import { init as initValidateForm, reset as resetPristine } from './form-validat
 import { reset as resetScaleControl } from './scaler.js';
 import { reset as resetEffect } from './effects.js';
 
+const FILE_TYPES = ['jpg', 'jpeg', 'png'];
+
 const formNode = document.querySelector('.img-upload__form');
 const imgUploadRedactorNode = formNode.querySelector('.img-upload__overlay');
 const imgUploadInputNode = formNode.querySelector('.img-upload__input');
+const imgUploadPreviewNode = formNode.querySelector('.img-upload__preview img');
 const closeFormNode = formNode.querySelector('.img-upload__cancel');
 
 const close = () => {
@@ -18,8 +21,20 @@ const close = () => {
   document.removeEventListener('keydown', onDocumentKeydown);
 };
 
+const showSelectImg = () => {
+  const file = imgUploadInputNode.files[0];
+  const fileName = file.name.toLowerCase();
+
+  const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
+
+  if (matches) {
+    imgUploadPreviewNode.src = URL.createObjectURL(file);
+  }
+};
+
 const open = () => {
   imgUploadInputNode.addEventListener('change', () => {
+    showSelectImg();
     imgUploadRedactorNode.classList.remove('hidden');
     document.body.classList.add('modal-open');
     document.addEventListener('keydown', onDocumentKeydown);
